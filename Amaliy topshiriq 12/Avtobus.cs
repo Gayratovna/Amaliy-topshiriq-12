2e1a110157e9982fe751e6b2c41441abb3a67c49
User <User@WIN-8BH8LKBRQNO>
2024-01-17 12:24:30 +0500

    Avtobus

---
+++ Amaliy topshiriq 12/Avtobus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amaliy_topshiriq_12
+{
+    public class Avtobus
+    {
+        public string Raqam { get; set; }
+        public string HaydovchiIsmi { get; set; }
+        public string YonalishRaqam { get; set; }
+    }
+}
